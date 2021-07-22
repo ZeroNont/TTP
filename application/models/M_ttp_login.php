dc044b641ff559e0c0c2035c9b07a2b5f92a5b12
Niphat <62160095@go.buu.ac.th>
2021-07-23 00:47:22 +0700

    edit M_ttp_login

--- application/models/M_ttp_login.php
+++ application/models/M_ttp_login.php
@@ -12,7 +12,7 @@ class M_ttp_login extends Da_ttp_login {
 	}
 	public function check_login($User_login,$Pass_login)
 	{//check username and password in database
-		$sql="SELECT*FROM user_login 
+		$sql="SELECT*FROM ttps_database.user_login 
 			WHERE User_login='$User_login' 
 			AND Pass_login = '$Pass_login'
 			";
