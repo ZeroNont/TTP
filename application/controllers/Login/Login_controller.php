15c9958b998e5e40de4b42372efc33ad9351469e
Niphat <62160095@go.buu.ac.th>
2021-07-25 23:42:35 +0700

    edit
    Login ส่งค่าrole

--- application/controllers/Login/Login_controller.php
+++ application/controllers/Login/Login_controller.php
@@ -18,11 +18,16 @@ class Login_controller extends MainController
 		$this->load->model('M_ttp_Emp','meng');
 		$this->meng->Emp_ID = $Enp_ID;
 		$data['Emp_ID'] = $this->meng->get_emp()->row();
+		
 		$temp = $data['Emp_ID'];
 		$this->session->set_userdata('UsEmp_ID', $temp->Emp_ID);
 		$this->session->set_userdata('UsName_EN', $temp->Empname_eng." ".$temp->Empsurname_eng);
 		$this->session->set_userdata('UsName_TH', $temp->Empname_th." ".$temp->Empsurname_th);
 		$this->session->set_userdata('UsDepartment', $temp->Department);
+		//$this->meng->User_Role = $Enp_ID;
+		//$data['User_Role'] = $this->meng->get_role()->row();
+		//$tempRole = $data['User_Role'];
+		$this->session->set_userdata('Usrole', $temp->User_Role);
 		$this->main();
 
 	}
@@ -48,8 +53,15 @@ class Login_controller extends MainController
 	public function main()
     {
         if (!empty($this->session->userdata('UsEmp_ID'))) {
-
-            redirect('Main/Main_controller/show_main', 'refresh');
+			if($_SESSION['Usrole']==1){
+            	redirect('Main/Main_controller/show_main', 'refresh');
+			}else if($_SESSION['Usrole']==2){
+				//redirect('Main/Main_controller/show_main', 'refresh');
+			}else if($_SESSION['Usrole']==3){
+				//redirect('Main/Main_controller/show_main', 'refresh');
+			}else if($_SESSION['Usrole']==4){
+				//
+			}
         }
         // if
         else {
--- application/models/M_ttp_Emp.php
+++ application/models/M_ttp_Emp.php
@@ -15,15 +15,23 @@ class M_ttp_Emp extends Da_ttp_Emp
     public function get_emp()
     {
         $sql = "SELECT * 
-    FROM dbmc.employee AS emp
-    INNER JOIN dbmc.group_secname AS gsec 
-    ON gsec.Sectioncode = emp.Sectioncode_ID
-    INNER JOIN dbmc.position AS pos
-    ON pos.Position_ID = emp.Position_ID
-    INNER JOIN dbmc.sectioncode AS sec
-    ON sec.Sectioncode = emp.Sectioncode_ID
-    WHERE emp.Emp_ID=?";
-$query = $this->db->query($sql,array($this->Emp_ID));
-return $query;
+            FROM dbmc.employee AS emp
+            INNER JOIN dbmc.group_secname AS gsec 
+            ON gsec.Sectioncode = emp.Sectioncode_ID
+            INNER JOIN dbmc.position AS pos
+            ON pos.Position_ID = emp.Position_ID
+            INNER JOIN dbmc.sectioncode AS sec
+            ON sec.Sectioncode = emp.Sectioncode_ID
+            INNER JOIN ttps_database.user_login AS ulog
+            ON emp.Emp_ID = ulog.Enp_ID
+            WHERE emp.Emp_ID=?";
+        $query = $this->db->query($sql,array($this->Emp_ID));
+        return $query;
     }
+    // function get_role(){
+    //     $sql = "SELECT User_Role 
+    //     FROM ttps_database.user_login";
+    //     $query = $this->db->query($sql);
+    //     return $query;
+    // }
 }
