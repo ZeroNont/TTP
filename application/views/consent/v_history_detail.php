2b64b72d7778d628d8eb1142753fb47d5c93512d
mitelice <73128722+Phatchara-AIB@users.noreply.github.com>
2021-07-26 20:47:48 +0700

    v_history_detail

--- application/views/consent/v_history_detail.php
+++ application/views/consent/v_history_detail.php
@@ -121,12 +121,12 @@
     </div>
 
 <!-- ========== ข้อมูลผู้พิจารณา ========== -->
-        <?php if ($arr_form->Status != 1 ) { ?>
+        <?php if ($arr_form->Status != 0) { ?>
             <div class="card-body">            
                 <h3>ข้อมูลผู้พิจารณา</h3>
                                 <div class="row">
                                     <!-- แสดงชื่อหัวหน้า อนุมัติ -->
-                                    <?php if ($arr_form->Status == 2 || $arr_form->Status == 3 || $arr_form->Status == 4 || $arr_form->Status == -2 || $arr_form->Status == -1 ) { ?>
+                                    <?php if ($arr_form->Status == 1 || $arr_form->Status == 2 || $arr_form->Status == 3 || $arr_form->Status == 4 || $arr_form->Status == -2 || $arr_form->Status == -1 ) { ?>
                                         <div class="col-6 col-md-4" id="card_align"><span><b>ชื่อผู้อนุมัติ : </b>
                                             <?php echo $arr_list->Empname_th . " " . $arr_list->Empsurname_th; ?>
                                             <br>
@@ -156,7 +156,7 @@
                                        <div class="col-6 col-md-4"><span><b>ชื่อผู้อนุมัติ : 
                                             <?php echo $arr_list->Empname_th . " " . $arr_list->Empsurname_th; ?>
                                             <br>
-                                            <b>ตำแหน่ง : </b>Approve Plant 
+                                            <b>ตำแหน่ง : </b>Approve Plant <br>
                                             <?php //แสดงตำแหน่งหัวหน้าแบบกำหนดเอง หรือดึงจากดาต้า
                                             $newDate = date("d/m/Y", strtotime($arr_list->Approval_plant_date)); ?>
                                             <br>
@@ -212,11 +212,12 @@
                                         <?php } ?>
                                     </div>
             </div>
-           
-        <?php  }?>
-        <br>
-                    <a href="<?php echo base_url().'history/ttp_history/show_history_employee/'.$_SESSION['UsEmp_ID']?>" >  
-                        <center><button type="button" class="btn btn-danger canter" id="button_size">กลับ</button> </center>
+            <br>
+        <?php } ?>
+
+        <a href="<?php echo base_url().'history/ttp_history/show_history_employee/'.$_SESSION['UsEmp_ID']?>" >  
+                        <center><button type="button" class="btn btn-danger canter" id="button_size">Back</button> </center>
                     </a>
             <br>
             <br>
+    
