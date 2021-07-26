bd0edfda285e8f30371ffb3679f4732d9bde17d1
62160326 <60688879+ZeroNont@users.noreply.github.com>
2021-07-26 22:20:39 +0700

    update_sidebar

--- application/views/includes/template/sidebar.php
+++ application/views/includes/template/sidebar.php
@@ -12,39 +12,43 @@
               <!-- Collapse -->
               <div class="collapse navbar-collapse" id="sidenav-collapse-main">
                   <!-- Nav items -->
+                  <?php $id = $_SESSION['Usrole'];
+                    if ($id == 1) { ?>
                   <ul class="navbar-nav">
                       <li class="nav-item ">
-                          <a class="nav-link " href="<?php  echo site_url() . 'Main/Main_controller/show_main/'?>">
+                          <a class="nav-link " href="<?php echo site_url() . 'Main/Main_controller/show_main/' ?>">
                               <i class="ni ni-tv-2 text-primary"></i>
                               <span class="nav-link-text">Home</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url(). 'licence_form/licence_input/index/'?>">
+                          <a class="nav-link" href="<?php echo base_url() . 'licence_form/licence_input/index/' ?>">
                               <i class="ni ni-badge text-orange"></i>
                               <span class="nav-link-text">Licence Form</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'Renewal/Renewal_controller/show_renewal/'?>">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'Renewal/Renewal_controller/show_renewal/' ?>">
                               <i class="ni ni-single-copy-04 text-primary"></i>
                               <span class="nav-link-text">Renewall Form</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'check_schedule/ttp_check_schedule/show_check_schedule/'?>">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'check_schedule/ttp_check_schedule/show_check_schedule/' ?>">
                               <i class="ni ni-calendar-grid-58 text-yellow"></i>
                               <span class="nav-link-text">Check Schedule</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'check_out/ttp_check_out/index/'?>">
+                          <a class="nav-link" href="<?php echo base_url() . 'check_out/ttp_check_out/index/' ?>">
                               <i class="ni ni-user-run text-default"></i>
                               <span class="nav-link-text">Check out</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'check_status/ttp_check_status/index/'?>">
+                          <a class="nav-link" href="<?php echo base_url() . 'check_status/ttp_check_status/index/' ?>">
                               <i class="ni ni-tag text-info"></i>
                               <span class="nav-link-text">Status Form</span>
                           </a>
@@ -52,41 +56,246 @@
 
 
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'history/ttp_history/show_history_employee/'?>">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'history/ttp_history/show_history_employee/' ?>">
                               <i class="ni ni-time-alarm text-pink"></i>
                               <span class="nav-link-text">History Form</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'request/ttp_request/show_request_list/'?>">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'request/ttp_request/show_request_list/' ?>">
                               <i class="ni ni-email-83 text-dark"></i>
                               <span class="nav-link-text">Requested</span>
                           </a>
                       </li>
 
                   </ul>
-                  <hr class="my-3">
+                  <?php } else if ($id == 2) { ?>
                   <ul class="navbar-nav">
-                      <li class="nav-item">
-                          <a class="nav-link" href="examples/upgrade.html">
-                              <i class="ni ni-active-40 text-red"></i>
-                              <span class="nav-link-text">Approve Form</span>
+                      <li class="nav-item ">
+                          <a class="nav-link " href="<?php echo site_url() . 'Main/Main_controller/show_main/' ?>">
+                              <i class="ni ni-tv-2 text-primary"></i>
+                              <span class="nav-link-text">Home</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'plan_management/plan_list/index/'?>">
-                              <i class="ni ni-single-02 text-green"></i>
-                              <span class="nav-link-text">Plant Management</span>
+                          <a class="nav-link" href="<?php echo base_url() . 'licence_form/licence_input/index/' ?>">
+                              <i class="ni ni-badge text-orange"></i>
+                              <span class="nav-link-text">Licence Form</span>
                           </a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="<?php echo base_url().'Report/Report_controller/show_report/'?>">
-                              <i class="ni ni-chart-bar-32 text-blue"></i>
-                              <span class="nav-link-text">Report</span>
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'Renewal/Renewal_controller/show_renewal/' ?>">
+                              <i class="ni ni-single-copy-04 text-primary"></i>
+                              <span class="nav-link-text">Renewall Form</span>
                           </a>
                       </li>
-                  </ul>
-                  <!-- Divider -->
+                      <li class="nav-item">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'check_schedule/ttp_check_schedule/show_check_schedule/' ?>">
+                              <i class="ni ni-calendar-grid-58 text-yellow"></i>
+                              <span class="nav-link-text">Check Schedule</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="<?php echo base_url() . 'check_out/ttp_check_out/index/' ?>">
+                              <i class="ni ni-user-run text-default"></i>
+                              <span class="nav-link-text">Check out</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="<?php echo base_url() . 'check_status/ttp_check_status/index/' ?>">
+                              <i class="ni ni-tag text-info"></i>
+                              <span class="nav-link-text">Status Form</span>
+                          </a>
+                      </li>
+
+
+                      <li class="nav-item">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'history/ttp_history/show_history_employee/' ?>">
+                              <i class="ni ni-time-alarm text-pink"></i>
+                              <span class="nav-link-text">History Form</span>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link"
+                              href="<?php echo base_url() . 'request/ttp_request/show_request_list/' ?>">
+                              <i class="ni ni-email-83 text-dark"></i>
+                              <span class="nav-link-text">Requested</span>
+                          </a>
+                      </li>
+
+                      <hr class="my-3">
+                      <ul class="navbar-nav">
+                          <li class="nav-item">
+                              <a class="nav-link" href="examples/upgrade.html">
+                                  <i class="ni ni-active-40 text-red"></i>
+                                  <span class="nav-link-text">Approve Form</span>
+                              </a>
+                          </li>
+
+
+                      </ul>
+                      <?php } else if ($id == 3) { ?>
+
+                      <ul class="navbar-nav">
+                          <li class="nav-item ">
+                              <a class="nav-link " href="<?php echo site_url() . 'Main/Main_controller/show_main/' ?>">
+                                  <i class="ni ni-tv-2 text-primary"></i>
+                                  <span class="nav-link-text">Home</span>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="<?php echo base_url() . 'licence_form/licence_input/index/' ?>">
+                                  <i class="ni ni-badge text-orange"></i>
+                                  <span class="nav-link-text">Licence Form</span>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link"
+                                  href="<?php echo base_url() . 'Renewal/Renewal_controller/show_renewal/' ?>">
+                                  <i class="ni ni-single-copy-04 text-primary"></i>
+                                  <span class="nav-link-text">Renewall Form</span>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link"
+                                  href="<?php echo base_url() . 'check_schedule/ttp_check_schedule/show_check_schedule/' ?>">
+                                  <i class="ni ni-calendar-grid-58 text-yellow"></i>
+                                  <span class="nav-link-text">Check Schedule</span>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link" href="<?php echo base_url() . 'check_out/ttp_check_out/index/' ?>">
+                                  <i class="ni ni-user-run text-default"></i>
+                                  <span class="nav-link-text">Check out</span>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link"
+                                  href="<?php echo base_url() . 'check_status/ttp_check_status/index/' ?>">
+                                  <i class="ni ni-tag text-info"></i>
+                                  <span class="nav-link-text">Status Form</span>
+                              </a>
+                          </li>
+
+
+                          <li class="nav-item">
+                              <a class="nav-link"
+                                  href="<?php echo base_url() . 'history/ttp_history/show_history_employee/' ?>">
+                                  <i class="ni ni-time-alarm text-pink"></i>
+                                  <span class="nav-link-text">History Form</span>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a class="nav-link"
+                                  href="<?php echo base_url() . 'request/ttp_request/show_request_list/' ?>">
+                                  <i class="ni ni-email-83 text-dark"></i>
+                                  <span class="nav-link-text">Requested</span>
+                              </a>
+                          </li>
+
+                          <hr class="my-3">
+                          <ul class="navbar-nav">
+                              <li class="nav-item">
+                                  <a class="nav-link" href="examples/upgrade.html">
+                                      <i class="ni ni-active-40 text-red"></i>
+                                      <span class="nav-link-text">Approve Form</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'plan_management/plan_list/index/' ?>">
+                                      <i class="ni ni-single-02 text-green"></i>
+                                      <span class="nav-link-text">Plant Management</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'Report/Report_controller/show_report/' ?>">
+                                      <i class="ni ni-chart-bar-32 text-blue"></i>
+                                      <span class="nav-link-text">Report</span>
+                                  </a>
+                              </li>
+                          </ul>
+                          <?php } else if ($id == 4) { ?>
+
+                          <ul class="navbar-nav">
+                              <li class="nav-item ">
+                                  <a class="nav-link "
+                                      href="<?php echo site_url() . 'Main/Main_controller/show_main/' ?>">
+                                      <i class="ni ni-tv-2 text-primary"></i>
+                                      <span class="nav-link-text">Home</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'licence_form/licence_input/index/' ?>">
+                                      <i class="ni ni-badge text-orange"></i>
+                                      <span class="nav-link-text">Licence Form</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'Renewal/Renewal_controller/show_renewal/' ?>">
+                                      <i class="ni ni-single-copy-04 text-primary"></i>
+                                      <span class="nav-link-text">Renewall Form</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'check_schedule/ttp_check_schedule/show_check_schedule/' ?>">
+                                      <i class="ni ni-calendar-grid-58 text-yellow"></i>
+                                      <span class="nav-link-text">Check Schedule</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'check_out/ttp_check_out/index/' ?>">
+                                      <i class="ni ni-user-run text-default"></i>
+                                      <span class="nav-link-text">Check out</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'check_status/ttp_check_status/index/' ?>">
+                                      <i class="ni ni-tag text-info"></i>
+                                      <span class="nav-link-text">Status Form</span>
+                                  </a>
+                              </li>
+
+
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'history/ttp_history/show_history_employee/' ?>">
+                                      <i class="ni ni-time-alarm text-pink"></i>
+                                      <span class="nav-link-text">History Form</span>
+                                  </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a class="nav-link"
+                                      href="<?php echo base_url() . 'request/ttp_request/show_request_list/' ?>">
+                                      <i class="ni ni-email-83 text-dark"></i>
+                                      <span class="nav-link-text">Requested</span>
+                                  </a>
+                              </li>
+
+                              <hr class="my-3">
+                              <ul class="navbar-nav">
+                                  <li class="nav-item">
+                                      <a class="nav-link" href="examples/upgrade.html">
+                                          <i class="ni ni-active-40 text-red"></i>
+                                          <span class="nav-link-text">Approve Form</span>
+                                      </a>
+                                  </li>
+
+
+                              </ul>
+                              <?php } ?>
+                              <!-- Divider -->
 
               </div>
 
