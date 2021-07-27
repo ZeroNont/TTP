6d80513894542304b0d7527ed9bf59c42158203d
jxcho <72914014+jxcho@users.noreply.github.com>
2021-07-27 17:16:35 +0700

    Appove form สำหรับ HR ที่ยังไม่ค่อยสมบูรณ์เท่าไหร่

---
+++ application/views/consent/v_approve_form_detail.php
@@ -0,0 +1,250 @@
+<script>
+$(document).ready(function() {
+    $("#btn_success").click(function() {
+        $("#Modal_approve").hide();
+
+    });
+
+});
+</script>
+
+<h1>
+    Request (อนุมัติคำร้องขออนุญาต)
+</h1>
+<div class="col-xl-12 order-xl-1">
+    <div class="card">
+
+        <div class="card-body ">
+
+            <div class="pl-lg-4">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-username">Start Date
+                                (วันที่เริ่มต้น)</label>
+
+                            <input type="date" name="Start_date" class="form-control" required
+                                value="<?php echo $arr_req->Start_date ?>">
+
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-email">End Date
+                                (วันที่สิ้นสุด)</label>
+                            <input type="date" name="End_date" class="form-control" required
+                                value="<?php echo $arr_req->End_date ?>">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-first-name">Item
+                                (สิ่งที่ต้องการวาง)</label>
+                            <input type="text" name="Item" class="form-control" require
+                                value="<?php echo $arr_req->Item ?>">
+                        </div>
+                    </div>
+                    <div class=" col-lg-12 ">
+                        <div class=" form-group">
+                            <label class="form-control-label" for="input-last-name">Reason
+                                (เหตุผลในการวาง)</label>
+                            <input type="text" name="Reason" class="form-control" require
+                                value="<?php echo $arr_req->Reason ?>">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="pl-lg-4">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-address">Officer in Charge
+                                (ผู้รับผิดชอบ)</label>
+                            <input class="form-control" type="text" name="Officer" require
+                                value="<?php echo $arr_emp->Empname_th.' '.$arr_emp->Empsurname_th ?>">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-city">Tell No.
+                                (เบอร์โทรศัพท์)</label>
+                            <input type="text" class="form-control" name="Tell" require
+                                value="<?php echo $arr_req->Tell ?>">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-country">Layout
+                                (รูปแบบการวาง)</label>
+                            <br>
+                            <a href="<?php echo base_url() ?>assets/file/layout/<?php echo $arr_req->Layout_location ?>"
+                                download>
+                                <button type="button" class="btn btn-danger" id="button_size">
+                                    <i class="fas fa-file-alt text-dark"></i>
+                                    &nbsp;&nbsp;File
+                                </button>
+                            </a>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label class="form-control-label" for="input-country">Plan
+                                (แผนการวาง)</label>
+                            <br>
+                            <a href="<?php echo base_url() ?>assets/file/Plan/<?php echo $arr_req->Layout_location ?>"
+                                download>
+                                <button type="button" class="btn btn-danger" id="button_size">
+                                    <i class="fas fa-file-alt text-dark"></i>
+                                    &nbsp;&nbsp;File
+                                </button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <hr>
+
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <h3>ข้อมูลพิจารณาคำขอ</h3>
+                    <h5>ชื่อ : <?php echo $arr_emp->Empname_th.' '.$arr_emp->Empsurname_th ?></h5>
+                    <h5>ตำแหน่ง : หัวหน้างาน</h5>
+                    <h5>สถานะ : อนุมัติ</h5>
+
+                </div>
+            </div>
+
+
+            <div class="row">
+
+                <div class="col-lg-4">
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-danger btn-lg float-right" data-toggle="modal"
+                        data-target="#Modal_reject">
+                        Reject
+                    </button>
+
+                </div>
+
+                <div class="col-lg-4">
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-success btn-lg float-right" data-toggle="modal"
+                        data-target="#Modal_approve">
+                        Approve
+                    </button>
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+</div>
+<!-- Modal -->
+<div class="modal fade" id="Modal_reject" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+
+                <label for="" class="form-control-label">
+
+                    กรุณากรอกเหตุผล
+                </label>
+
+
+
+                <form method="POST" action="<?php echo site_url().'/approve_form/ttp_approve_form/insert_reason/'; ?>">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <input type="text" name="reject_reason" class="form-control"
+                                    placeholder="เหตุผลในการปฏิเสธ" require>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger btn-lg float-right" data-dismiss="modal">Close</button>
+
+                <a href=" <?php echo site_url() . '/approve_form/ttp_approve_form/insert_reason/' ; ?>">
+                    <button type="submit" class="btn btn-success btn-lg float-right">Submit</button>
+                </a>
+            </div>
+
+            </form>
+
+
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="Modal_approve" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" align="center">
+                <h1 class="modal-title" id="ModalLabel">ยืนยันการอนุมัติคำขอ</h1>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger btn-lg float-right" data-dismiss="modal">Close</button>
+
+                <!-- Button trigger modal -->
+
+                <button type="button" class="btn btn-success btn-lg float-right" id="btn_success" data-toggle="modal"
+                    data-target="#successModal">
+                    Submit
+                </button>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- end modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+
+            <div class="modal-body" align="center">
+                <h1> อนุมัติคำขอสำเร็จ </h1>
+            </div>
+            <div class="modal-footer">
+                <a
+                    href="<?php echo site_url() . 'approve_form/ttp_approve_form/update_approve_form_form/'. $arr_req->Form_ID ; ?>">
+                    <button type="button" class="btn btn-success btn-lg float-right">Agree</button>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end modal  -->
