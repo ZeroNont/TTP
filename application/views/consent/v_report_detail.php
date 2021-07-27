69042bfad1092034c1f1541b75bc6a40bb5942a8
Chakrit B <62160148@go.buu.ac.th>
2021-07-28 02:00:38 +0700

    Update v_report_detail.php

--- application/views/consent/v_report_detail.php
+++ application/views/consent/v_report_detail.php
@@ -9,12 +9,13 @@
 */ -->
 
 <a href="<?php echo site_url() . 'Report/Report_controller/show_report'; ?>" class="btn btn-secondary float-left"><i class="fas fa-arrow-alt-circle-left"></i> Back</a>
+<a href='#' id='download_link' onClick='javascript:ExcelReport();' class="btn btn-primary float-right"><i class="fa fa-download"></i> Export Excel</a>
 
 <h1>
     &nbsp; Report (รายงานข้อมูล)
 </h1>
 <br>
-<div class="card-header" id="card_radius">
+<div class="card-header">
     <h1 style="text-align:center">
         Running No. <?php echo $Form_data->HR_No; ?>
         <br>
@@ -44,7 +45,76 @@
         <b>Status :</b> <?php echo $Status; ?><br>
     </h2>
 
+    <br>
+    &nbsp;
 
+    <div class="row" id="count_table">
+        <div class="col-xl-12">
+            <div class="card">
+                <div class="table-responsive" table id='myTable'>
+                    <table class="table align-items-center table-flush">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">Running No.</th>
+                                <th scope="col">Company</th>
+                                <th scope="col">Requester</th>
+                                <th scope="col">Plant No.</th>
+                                <th scope="col">Plant Name.</th>
+                                <th scope="col">Reason</th>
+                                <th scope="col">Requested date</th>
+                                <th scope="col">Approval date</th>
+                                <th scope="col">Starting date</th>
+                                <th scope="col">End date</th>
+                                <th scope="col">Approver</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody id="data_table">
+                            <tr>
+                                <td><?php echo $Form_data->HR_No; ?></td>
+                                <td><?php echo $Form_data->Company_name . ' ' . "(" .  $Form_data->Company_name_th . ")" ?></td>
+                                <td><?php echo $Form_data->Officer; ?></td>
+                                <td><?php echo $Form_data->Plant_No; ?></td>
+                                <td><?php echo $Form_data->Plant_name; ?></td>
+                                <td><?php echo $Form_data->Reason; ?></td>
+                                <td><?php echo date("d-m-Y", strtotime($Form_data->Requested_date)); ?></td>
+                                <td><?php echo date("d-m-Y", strtotime($Form_data->Approve_date)) ?></td>
+                                <td><?php echo date("d-m-Y", strtotime($Form_data->Start_date)) ?></td>
+                                <td><?php echo date("d-m-Y", strtotime($Form_data->End_date)) ?></td>
+                                <td><?php echo $Form_data->Empname_engTitle . ' ' . $Form_data->Empname_eng . ' ' . $Form_data->Empsurname_eng; ?></td>
+                                <?php
+                                if ($Form_data->Status == '4') {
+                                    $Status = 'ยังอยู่ในคลัง';
+                                } else if ($Form_data->Status > '4') {
+                                    $Status = 'สิ้นสุดการวาง';
+                                } else if ($Form_data->Status < '4') {
+                                    $Status = 'รอการอนุมัติ';
+                                }
+                                ?>
+                                <td><?php echo $Status; ?></td>
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<br>
-&nbsp;
+
+<script src="https://unpkg.com/xlsx/dist/xlsx.full.min.js"></script>
+<script src="https://unpkg.com/file-saver@1.3.3/FileSaver.js"></script>
+
+<script>
+    function ExcelReport() //function สำหรับสร้าง ไฟล์ excel จากตาราง
+    {
+        var sheet_name = "Report"; /* กำหหนดชื่อ sheet ให้กับ excel โดยต้องไม่เกิน 31 ตัวอักษร */
+        var elt = document.getElementById('myTable'); /*กำหนดสร้างไฟล์ excel จาก table element ที่มี id ชื่อว่า myTable*/
+
+        /*------สร้างไฟล์ excel------*/
+        var wb = XLSX.utils.table_to_book(elt, {
+            sheet: sheet_name
+        });
+        XLSX.writeFile(wb, 'SDM&SKD Temporary Tag Permission Report.xlsx'); //Download ไฟล์ excel จากตาราง html โดยใช้ชื่อว่า SDM&SKD Temporary Tag Permission Report.xlsx
+    }
+</script>
