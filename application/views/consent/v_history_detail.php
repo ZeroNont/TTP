e18aa52578f9b2cf87d18416a2da36e4115cd3b5
Pantakon <73054301+Pantakon@users.noreply.github.com>
2021-07-30 14:47:09 +0700

    Update v_history_detail.php

--- application/views/consent/v_history_detail.php
+++ application/views/consent/v_history_detail.php
@@ -89,7 +89,7 @@
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    <label class="form-control-label" for="input-city">Tell No.
+                                    <label class="form-control-label" for="input-city">Tel No.
                                         (เบอร์โทรศัพท์)</label>
                                     <input type="text" class="form-control" name="Tell" require value="<?php echo $arr_form->Tell ?>" disabled>
                                 </div>
