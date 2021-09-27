f246423feba9483e80989f39cdba5525abe9bcfb
62160326 <60688879+ZeroNont@users.noreply.github.com>
2021-09-27 14:43:55 +0700

    เพิ่ม status plant

--- application/views/consent/v_plant_list.php
+++ application/views/consent/v_plant_list.php
@@ -58,169 +58,188 @@ function getEmp_edit(i) {
     });
 }
 </script>
-<h1>
-     Plan Management (การจัดการข้อมูล Approve Plant)
-    <button class="btn btn-primary float-right" data-toggle="modal" data-target="#Add"><i class="material-icons">Add
-            Approve
-            Plan</i></button>
-</h1>
-<div id="Add" class="modal fade" role="dialog">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header ">
+<div class="card-header" id="card_radius">
+    <h1>
+        Plant Management (การจัดการข้อมูล Approve Plant)
+        <button class="btn btn-primary float-right" data-toggle="modal" data-target="#Add"><i class="material-icons">Add
+                Approve
+                Plant</i></button>
+    </h1>
+    <div id="Add" class="modal fade" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header ">
 
-                <h4 align='center' class="modal-title" id="exampleModalLabel">
+                    <h4 align='center' class="modal-title" id="exampleModalLabel">
 
-                    Add Detail
-                </h4>
+                        Add Detail
+                    </h4>
 
-                <button type=" button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                    <button type=" button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <form action="<?php echo site_url() . 'Plant_management/Plant_input/insert'; ?>" method="post"
+                        enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="focusedinput" class="form-label">Employee ID</label>
+                            <input type="text" class="form-control" name="Emp_ID" id="Emp_id_modal"
+                                placeholder="JS000xxx" onkeyup="get_Emp()" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="focusedinput" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="showname_modal" disabled name="Plant_name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Plant No.</label>
+                            <input type="text" class="form-control" name="Plant_No" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Plant Name</label>
+                            <input type="text" class="form-control" name="Plant_name" required>
+                        </div>
+                        <button type="submit" class="btn btn-success float-right">Submit</button>
+                        <button type="button" class="btn btn-danger float-right" data-dismiss="modal">Cancel</button>
+                    </form>
+                </div>
+
             </div>
-
-            <div class="modal-body">
-                <form action="<?php echo site_url() . 'Plant_management/Plant_input/insert'; ?>" method="post"
-                    enctype="multipart/form-data">
-                    <div class="mb-3">
-                        <label for="focusedinput" class="form-label">Employee ID</label>
-                        <input type="text" class="form-control" name="Emp_ID" id="Emp_id_modal" placeholder="JS000xxx"
-                            onkeyup="get_Emp()" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="focusedinput" class="form-label">Name</label>
-                        <input type="text" class="form-control" id="showname_modal" disabled name="Plant_name">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Plant No.</label>
-                        <input type="text" class="form-control" name="Plant_No" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Plant Name</label>
-                        <input type="text" class="form-control" name="Plant_name" required>
-                    </div>
-                    <button type="submit" class="btn btn-success float-right">Submit</button>
-                    <button type="button" class="btn btn-danger float-right" data-dismiss="modal">Cancel</button>
-                </form>
-            </div>
-
         </div>
     </div>
-</div>
-<div class="card-header" id="card_radius">
-    <div class="table-responsive">
+    <div class="card-header" id="card_radius">
+        <div class="table-responsive">
 
-        <table class="table" id="example">
-            <thead class="thead-light">
-                <tr>
-                    <th>#</th>
-                    <th>Plan No.</th>
-                    <th>Plan Name</th>
-                    <th>Name</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php for ($i = 0; $i < count($obj_plan); $i++) { ?>
-                <tr>
-                    <td>
-                        <?php echo $i + 1 ?>
-                    </td>
-                    <td>
-                        <?php echo $obj_plan[$i]->pla_plant_no ?>
-                    </td>
-                    <td>
-                        <?php echo $obj_plan[$i]->pla_plant_name ?>
-                    </td>
-                    <td>
-                        <?php echo $obj_plan[$i]->Empname_eng . '            ' . $obj_plan[$i]->Empsurname_eng ?>
-                    </td>
-                    <td>
+            <table class="table" id="example">
+                <thead class="thead-light">
+                    <tr>
+                        <th>#</th>
+                        <th>Plant No.</th>
+                        <th>Plant Name</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php for ($i = 0; $i < count($obj_plan); $i++) { ?>
+                    <tr>
+                        <td>
+                            <?php echo $i + 1 ?>
+                        </td>
+                        <td>
+                            <?php echo $obj_plan[$i]->pla_plant_no ?>
+                        </td>
+                        <td>
+                            <?php echo $obj_plan[$i]->pla_plant_name ?>
+                        </td>
+                        <td>
+                            <?php echo $obj_plan[$i]->Empname_eng . '            ' . $obj_plan[$i]->Empsurname_eng ?>
+                        </td>
+                        <td>
+                            <span class="badge badge-dot mr-4">
+                                <?php if ($obj_plan[$i]->pla_status == 1) { ?>
+                                <i class="bg-success"></i>
+                                <span class="status">out of process</span>
+                                <?php } else if ($obj_plan[$i]->pla_status != 1) { ?>
+                                <i class="bg-warning"></i>
+                                <span class="status">In process</span>
+                                <?php } ?>
+                            </span>
+                        </td>
+                        <td>
+                            <?php if ($obj_plan[$i]->pla_status == 1) { ?>
+
+                            <button class="btn btn-warning" data-toggle="modal"
+                                data-target="#exampleModal<?php echo $i; ?>"><i class="material-icons">Edit</i></button>
+                            <!-- Modal -->
 
 
-                        <button class="btn btn-warning" data-toggle="modal"
-                            data-target="#exampleModal<?php echo $i; ?>"><i class="material-icons">Edit</i></button>
-                        <!-- Modal -->
+                            <div id="exampleModal<?php echo $i; ?>" class="modal fade" role="dialog">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header ">
 
+                                            <h4 class="modal-title" id="exampleModalLabel">
 
-                        <div id="exampleModal<?php echo $i; ?>" class="modal fade" role="dialog">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header ">
+                                                Edit Detail
+                                            </h4>
 
-                                        <h4 class="modal-title" id="exampleModalLabel">
+                                            <button type=" button" class="close" data-dismiss="modal"
+                                                aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
 
-                                            Edit Detail
-                                        </h4>
+                                        <div class="modal-body">
+                                            <form
+                                                action="<?php echo site_url() . 'Plant_management/Plant_input/edit/' . $obj_plan[$i]->Emp_ID; ?>"
+                                                method="post" enctype="multipart/form-data">
+                                                <div class="mb-3">
+                                                    <label for="exampleInputEmail1" class="form-label">Employee
+                                                        ID</label>
+                                                    <input type="text" class="form-control"
+                                                        value="<?php echo $obj_plan[$i]->pla_emp_id ?>"
+                                                        id="Emp_id<?php echo $i; ?>"
+                                                        onkeyup="getEmp_edit(<?php echo $i; ?>)" name="Emp_ID" required>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleInputPassword1" class="form-label">Name</label>
+                                                    <input type="text" class="form-control"
+                                                        value="<?php echo $obj_plan[$i]->Empname_eng . '    ' . $obj_plan[$i]->Empsurname_eng ?>"
+                                                        disabled id="Emp_name<?php echo $i; ?>">
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleInputPassword1" class="form-label">Plant
+                                                        No.</label>
+                                                    <input type="text" class="form-control"
+                                                        value="<?php echo $obj_plan[$i]->pla_plant_no ?>"
+                                                        name="Plant_No" required>
+                                                    <input type="text" class="form-control"
+                                                        value="<?php echo $obj_plan[$i]->pla_plant_id ?>"
+                                                        name="Plant_ID" hidden>
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label for="exampleInputPassword1" class="form-label">Plant
+                                                        Name</label>
+                                                    <input type="text" class="form-control"
+                                                        value="<?php echo $obj_plan[$i]->pla_plant_name ?>"
+                                                        id="province_auto" name="Plant_name" required>
+                                                </div>
+                                                <button type="submit"
+                                                    class="btn btn-success float-right">Submit</button>
+                                                <button type="button" class="btn btn-danger float-right"
+                                                    data-dismiss="modal">Cancel</button>
+                                            </form>
+                                        </div>
 
-                                        <button type=" button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
                                     </div>
-
-                                    <div class="modal-body">
-                                        <form
-                                            action="<?php echo site_url() . 'Plant_management/Plant_input/edit/' . $obj_plan[$i]->Emp_ID; ?>"
-                                            method="post" enctype="multipart/form-data">
-                                            <div class="mb-3">
-                                                <label for="exampleInputEmail1" class="form-label">Employee
-                                                    ID</label>
-                                                <input type="text" class="form-control"
-                                                    value="<?php echo $obj_plan[$i]->pla_emp_id ?>"
-                                                    id="Emp_id<?php echo $i; ?>"
-                                                    onkeyup="getEmp_edit(<?php echo $i; ?>)" name="Emp_ID" required>
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="exampleInputPassword1" class="form-label">Name</label>
-                                                <input type="text" class="form-control"
-                                                    value="<?php echo $obj_plan[$i]->Empname_eng . '    ' . $obj_plan[$i]->Empsurname_eng ?>"
-                                                    disabled id="Emp_name<?php echo $i; ?>">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="exampleInputPassword1" class="form-label">Plant
-                                                    No.</label>
-                                                <input type="text" class="form-control"
-                                                    value="<?php echo $obj_plan[$i]->pla_plant_no ?>" name="Plant_No"
-                                                    required>
-                                                <input type="text" class="form-control"
-                                                    value="<?php echo $obj_plan[$i]->pla_plant_id ?>" name="Plant_ID"
-                                                    hidden>
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="exampleInputPassword1" class="form-label">Plant
-                                                    Name</label>
-                                                <input type="text" class="form-control"
-                                                    value="<?php echo $obj_plan[$i]->pla_plant_name ?>"
-                                                    id="province_auto" name="Plant_name" required>
-                                            </div>
-                                            <button type="submit" class="btn btn-success float-right">Submit</button>
-                                            <button type="button" class="btn btn-danger float-right"
-                                                data-dismiss="modal">Cancel</button>
-                                        </form>
-                                    </div>
-
                                 </div>
                             </div>
-                        </div>
-                    </td>
-                </tr>
-                <?php  } ?>
-            </tbody>
-        </table>
-        <div>
+                            <?php } else { ?>
+
+                            <button class="btn btn-secondary"><i class="material-icons">Edit</i></button>
+                            <?php } ?>
+                        </td>
+                    </tr>
+                    <?php  } ?>
+                </tbody>
+            </table>
+            <div>
+            </div>
         </div>
-    </div>
-    <script>
-    $(document).ready(function() {
-        $('#example').DataTable();
-    });
-    </script>
-    </script>
-    <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
-    <script src="../../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../../assets/vendor/js-cookie/js.cookie.js"></script>
-    <script src="../../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-    <script src="../../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
-    <!-- Argon JS -->
-    <script src="../../assets/js/argon.js?v=1.2.0"></script>
-    <script type="text/javascript">
+        <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+        </script>
+        </script>
+        <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
+        <script src="../../assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="../../assets/vendor/js-cookie/js.cookie.js"></script>
+        <script src="../../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+        <script src="../../assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+        <!-- Argon JS -->
+        <script src="../../assets/js/argon.js?v=1.2.0"></script>
+        <script type="text/javascript">
