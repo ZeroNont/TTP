a56c242c9b94dbc4649a4eddc9b6460f0496c63c
[Phatchara_62160159] <62160159@go.buu.ac.th>
2021-10-12 22:08:38 +0700

    Example test css & html
    งานที่พี่อ๊อฟให้ทำส่ง

---
+++ application/controllers/62160159/Exaple_test.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* 62160159
+* Controller
+* @input  - 
+* @output -
+* @author Phatchara
+* @Create Date 2564-10-12
+*/
+?>
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once(dirname(__FILE__) . "/../MainController.php");
+
+class Exaple_test extends MainController
+{
+
+    /*
+    * index
+    * display page v_62160159
+    * @input  - 
+    * @output -
+    * @author Phatchara
+    * @Create Date 2564-10-12
+    */
+    function index()
+    {
+        $this->output('consent/v_62160159');
+    }
+}
