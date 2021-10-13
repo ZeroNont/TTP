7124e1f07608adf451329a70d83f2712e8f65f0b
Natthakloy <73154307+Nattha23@users.noreply.github.com>
2021-10-13 21:45:18 +0700

    Example_test

---
+++ application/controllers/62160083/Example_test.php
@@ -0,0 +1,25 @@
+* @author Natthanit
+* @Create Date 2564-10-12
+*/
+-->
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+require_once(dirname(__FILE__) . "/../MainController.php");
+
+class Example_test extends MainController
+{
+
+    /*
+    * index
+    * display page v_62160083
+    * @input  -
+    * @output -
+    * @author Natthanit
+    * @Create Date 2564-10-12
+    */
+    function index()
+    {
+        $this->output('consent/v_62160083');
+    }
+}
